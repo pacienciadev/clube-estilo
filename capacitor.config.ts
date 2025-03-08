5c3deb53284079f5fb4caf82b833b11ccfc45436
Rafael Paciencia <rafapaci@gmail.com>
2025-03-08 01:55:55 -0300

    feat: Firebase emulator config ✨

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'onds-webapp',
-  webDir: 'dist'
+  appId: "io.ionic.starter",
+  appName: "clube-estilo-webapp",
+  webDir: "dist",
 };
 
 export default config;

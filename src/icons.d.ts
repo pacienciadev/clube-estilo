af3a2eb95d469f4b5e2b43805257a5d3a5753ed6
Rafael Paciencia <rafapaci@gmail.com>
2025-08-22 23:36:29 -0300

    ✨Update to build in cloud refact: Migrate from firebaze to azure fix: Icon types warnings fix: missing information

---
+++ src/icons.d.ts
@@ -0,0 +1,19 @@
+declare module "ionicons/icons" {
+  const ellipse: string;
+  const square: string;
+  const triangle: string;
+  const lockClosed: string;
+  const logoGoogle: string;
+  const personCircle: string;
+  const enterOutline: string;
+  
+  export {
+    ellipse,
+    square,
+    triangle,
+    lockClosed,
+    logoGoogle,
+    personCircle,
+    enterOutline,
+  };
+}

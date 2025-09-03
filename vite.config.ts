7c9a154d17cd11e66d68c7a4ec2769b6737715d4
Rafael Paciencia <rafapaci@gmail.com>
2025-09-02 21:54:10 -0300

    Production configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
+import { VitePWA } from "vite-plugin-pwa";
+
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [react(), legacy()],
+    plugins: [react(), legacy(), VitePWA({ registerType: "autoUpdate" })],
     test: {
       globals: true,
       environment: "jsdom",
